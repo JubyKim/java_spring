5a8d310982246b0d6fdfd71910015dfa45cc396c
JubyKim <uon9816@naver.com>
2022-08-09 11:27:11 +0900

    ~ch2

---
+++ Chapter1/src/ch04/ch04.java
@@ -0,0 +1,19 @@
+package ch04;
+
+public class ch04 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		int bNum = 0B1010; // 앞에 0B 2진수 
+		int oNum = 012; //앞에 0 : 8진수 
+		int xNum = 0xA; // 앞에 x0 : 16진수 
+		
+		
+		System.out.println(num);
+		System.out.println(bNum);
+		System.out.println(oNum);
+		System.out.println(xNum);
+		
+	}
+
+}
