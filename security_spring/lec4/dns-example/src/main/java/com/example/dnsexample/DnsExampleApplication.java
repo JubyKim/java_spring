067972fa899e7a6ac63af3feb520bfef6c0fd7f4
JubyKim <uon9816@naver.com>
2022-10-07 09:09:21 +0900

    todayis friday! yeah!

---
+++ security_spring/lec4/dns-example/src/main/java/com/example/dnsexample/DnsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnsExampleApplication.class, args);
+    }
+
+}
---
+++ security_spring/lec4/dns-example/src/test/java/com/example/dnsexample/DnsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dnsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
